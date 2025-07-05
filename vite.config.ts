580264d470a7ac8a7fe7ca71e05e05e2209ff2d1
Viktoriia_Kytsenko <vikakitsenko@gmail.com>
2025-07-05 19:19:50 +0200

    fixed name project

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/",
+  base: '/02-react-cafe/',
   plugins: [react()],
   build: {
     sourcemap: true,
