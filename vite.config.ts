643c7d6676c5a9b7f4b8ada59dec68dfb5bf542d
Viktoriia_Kytsenko <vikakitsenko@gmail.com>
2025-07-05 19:04:58 +0200

    Fix Vite base for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/02-react-cafe/",
+  base: "/",
   plugins: [react()],
   build: {
     sourcemap: true,
